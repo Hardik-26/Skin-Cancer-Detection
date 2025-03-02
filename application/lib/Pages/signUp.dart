e2a86e1050a655f81e483e53c031965a8b56302c
Himanshi Agarwal <himanshiagarwal1408@gmail.com>
2025-03-02 10:59:21 +0530

    Sign Up Frontend Complete with validation

--- application/lib/Pages/signUp.dart
+++ application/lib/Pages/signUp.dart
@@ -3,17 +3,18 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
-import 'package:can_scan/Pages/home.dart';
-import 'package:can_scan/Pages/signUp.dart';
+import 'package:can_scan/Pages/login.dart';
 
-class Login extends StatefulWidget {
-  const Login({super.key});
+class Signup extends StatefulWidget {
+  const Signup({super.key});
   @override
-  _login createState() => _login();
+  _signUp createState() => _signUp();
 }
 
-class _login extends State<Login> {
+class _signUp extends State<Signup> {
   // Variables to store input
+  String firstName = '';
+  String lastName = '';
   String phoneNumber = '';
   List<TextEditingController> otpControllers = List.generate(4, (index) => TextEditingController());
   List<FocusNode> otpFocusNodes = List.generate(4, (index) => FocusNode());
@@ -32,11 +33,11 @@ class _login extends State<Login> {
       appBar: AppBar(
         toolbarHeight: 80, // Increases the height of the AppBar
         titleSpacing: 20, // Adds spacing to the title
-        automaticallyImplyLeading: false,
+        iconTheme: IconThemeData(color: Colors.white),
         title: Padding(
           padding: EdgeInsets.symmetric(vertical: 10), // Adds space from top and bottom
           child: Text(
-            'Login',
+            'Sign Up',
             style: TextStyle(fontFamily: 'BaskervvilleSC'),
           ),
         ),
@@ -51,10 +52,10 @@ class _login extends State<Login> {
             padding: const EdgeInsets.all(8.0),
             child: GestureDetector(
               onTap: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context) => Signup()));
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Login()));
               },
               child: Text(
-                "Don't Have An Account? Sign Up",
+                "Already Have An Account? Login",
                 style: TextStyle(
                   color: Colors.white,
                 ),
@@ -70,8 +71,38 @@ class _login extends State<Login> {
         children: [
           Padding(padding: const EdgeInsets.symmetric(vertical: 25)),
           Padding(
-            padding: const EdgeInsets.only(bottom: 70),
-            child: Image.asset('assets/floating_icon.png',height: 130,width: 130)),
+              padding: const EdgeInsets.only(bottom: 50),
+              child: Image.asset('assets/floating_icon.png',height: 130,width: 130)),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  firstName = value;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your First Name',
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+            child: TextField(
+              onChanged: (value) {
+                setState(() {
+                  lastName = value;
+                });
+              },
+              decoration: InputDecoration(
+                hintText: 'Enter your Last Name',
+                border: OutlineInputBorder(),
+                contentPadding: EdgeInsets.symmetric(horizontal: 20, vertical: 15),
+              ),
+            ),
+          ),
           Padding(
             padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: TextField(
@@ -91,14 +122,26 @@ class _login extends State<Login> {
             padding: const EdgeInsets.all(20),
             child: ElevatedButton(
               onPressed: () {
+                if (firstName.isEmpty && lastName.isEmpty  && phoneNumber.isEmpty ) {
+                  showMessage("Enter all fields!");
+                  return;
+                }
+                if (firstName.isEmpty) {
+                  showMessage(" Enter your First Name!");
+                  return;
+                }
+                if (lastName.isEmpty) {
+                  showMessage(" Enter your Last Name!");
+                  return;
+                }
                 if (phoneNumber.isEmpty) {
-                  showMessage("Enter your phone number!");
+                  showMessage(" Enter your phone number!");
                   return;
                 }
                 setState(() {
                   showOtpFields = true;
                 });
-                performLogin(phoneNumber);
+                performSignUp(firstName, lastName, phoneNumber);
               },
               child: Padding(
                 padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
@@ -143,7 +186,7 @@ class _login extends State<Login> {
                 },
                 child: Padding(
                   padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 15),
-                  child: Text('Verify OTP'),
+                  child: Text('Sign Up'),
                 ),
               ),
             ),
@@ -152,9 +195,11 @@ class _login extends State<Login> {
     );
   }
 
-  // Example login function
-  void performLogin(String phoneNumber) async {
+  // Example signup function
+  void performSignUp(String firstName, String lastName, String phoneNumber) async {
     try {
+      print('First: $firstName');
+      print('Last: $lastName');
       print('Phone Number: $phoneNumber');
       generatedOTP = generateOTP();
       print('Generated OTP: $generatedOTP');
@@ -176,7 +221,7 @@ class _login extends State<Login> {
       //   );
       // }
     } catch (e) {
-      print('Error during login: $e');
+      print('Error during Sign Up: $e');
     }
   }
 
@@ -195,9 +240,9 @@ class _login extends State<Login> {
     }
 
     if (int.tryParse(enteredOTP) == generatedOTP) {
-      showMessage("Login Successfully!");
+      showMessage("Signed Up Successfully!");
     } else {
-      showMessage("Incorrect OTP,  try again!");
+      showMessage("Incorrect OTP, try again!");
     }
   }
 
